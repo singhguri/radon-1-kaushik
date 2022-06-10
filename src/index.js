92fdc64cbe437749b117134c2b5af433137ee7d4
singhguri <singhguri5598@gmail.com>
2022-06-11 04:59:09 +0530

    added middleware

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+const requestIp = require('request-ip');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const middleWare = (req, res, next) => {
+    console.log( new Date().toLocaleDateString(), requestIp.getClientIp(req), req.path);
+    next();
+}
+
+app.use(middleWare);
 
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
